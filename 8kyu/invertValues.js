8e48b00de11a4f4b0850210bea2423cacc3a10f6
jadenjoel <mailtojadenjoel76@gmail.com>
2022-11-04 02:49:58 +0000

    Add coding challenge for Nov 04 22

---
+++ 8kyu/invertValues.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+// MY SOLUTUTION :
+
+function invert(array) {
+  let arr = [];
+
+  for (let i = 0; i <= array.length; i++) {
+    const indNum = array[i];
+
+    if (indNum > 0) {
+      arr[i] = -indNum;
+    } else if (indNum <= 0) {
+      arr[i] = indNum * -1;
+    }
+  }
+  return arr;
+}
