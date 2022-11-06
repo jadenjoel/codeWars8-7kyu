d912ed4d13792276b93311c29a29e9e4589c7b11
jadenjoel <mailtojadenjoel76@gmail.com>
2022-11-06 02:52:21 +0000

    Add coding challenge for Nov 06 22

---
+++ 8kyu/booleanToString.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// MY SOLUTUTION :
+
+function booleanToString(b) {
+  return b.toString();
+}
