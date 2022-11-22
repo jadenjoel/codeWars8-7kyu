ded890a2005c6437c224005029acdd84815eafc0
jadenjoel <mailtojadenjoel76@gmail.com>
2022-11-22 03:47:19 +0000

    Add coding challenge for Nov 22 22

---
+++ 8kyu/multiply.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// This code does not execute properly. Try to figure out why.
+
+// MY SOLUTUTION : (needed to add the missing 'return' keyword)
+
+function multiply(a, b) {
+  return a * b;
+}
