ecb0af210905f785d5f22864d5675ec912a753c2
jadenjoel <mailtojadenjoel76@gmail.com>
2022-12-12 04:23:08 +0000

    Add coding challenge for Dec 12 22

---
+++ 8kyu/MakeUpperCase.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Write a function which converts the input string to uppercase.
+
+// MY SOLUTUTION :
+
+function makeUpperCase(str) {
+  const upper = str.toUpperCase();
+  return upper;
+}
