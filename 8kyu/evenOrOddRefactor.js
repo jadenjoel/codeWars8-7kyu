ca6e6c5d463596a7021d4d92129413eb25580970
jadenjoel <mailtojadenjoel76@gmail.com>
2023-01-18 17:11:25 +0000

    Add coding challenge for Jan 18 23

---
+++ 8kyu/evenOrOddRefactor.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// MY SOLUTUION  :
+
+function even_or_odd(number) {
+  //Takes in number -
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
